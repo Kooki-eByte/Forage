93517101c9c1846d1b5c4c2b6a662235340a5b33
Cristian Hornedo <officialcristianl@gmail.com>
2020-09-24 21:35:35 -0400

    Added in handlebars and deleted all of the regular html from the publics directory

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,11 +6,10 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.render("index");
   });
 
   app.get("/login", (req, res) => {
@@ -18,16 +17,16 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.render("members");
   });
 
   app.get("/food-search", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/food-search.html"));
+    res.render("food-search");
   });
 };
