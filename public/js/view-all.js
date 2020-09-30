694640cc5418e64618472cefca6f965954c9ad4f
Cristian Hornedo <officialcristianl@gmail.com>
2020-09-30 17:00:30 -0400

    Working on the view-all page

--- public/js/view-all.js
+++ public/js/view-all.js
@@ -1,3 +1,12 @@
 $(document).ready(() => {
+  function displayFood() {
+    let category = $(this).data("name");
+    let userId = $(this).data("user");
+    $.get(`/api/meals/${category}?users_id=${userId}`, function(data) {
+        // GONNA HAVE TO DO IT LIKE I DID BEFORE BY MAKING FOR LOOPS AND ASSIGNING THE SPECIFIC DATA TO THE CARDS . CANT DO A #EACH BECAUSE THE RES.RENDER WILL NOT WORK BECAUSE IT GRABBING WEIRD DATA. ALSO THE ROUTE WILL STAY THAT WEIRD API ROUTE SO IT WILL HAVE THE PAGE LOOK WEIRD
+      console.log(data);
+    });
+  }
 
+  $(document).on("click", "#display", displayFood);
 });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
   app.get("/signup", (req, res) => {
     res.render("signup");
   });
-  
+
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
@@ -21,12 +21,10 @@ module.exports = function(app) {
     res.render("login");
   });
 
-  
-
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    console.log(req.user)
+    console.log(req.user);
     res.render("members", req.user);
   });
 
@@ -37,5 +35,4 @@ module.exports = function(app) {
   app.get("/view-all", isAuthenticated, (req, res) => {
     res.render("view-all", req.user);
   });
-
 };
